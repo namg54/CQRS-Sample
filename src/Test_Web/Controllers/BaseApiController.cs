5ea8ddd1255112df828eccc3fe629df3fdf5f417
namg54 <79947165+namg54@users.noreply.github.com>
2023-09-01 17:04:58 +0330

    Add project files.

---
+++ src/Test_Web/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test_Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+      
+    }
+}
---
+++ src/Test_Web/Controllers/ProductsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test_Web.Controllers
+{
+    public class ProductsController : BaseApiController
+    {
+       
+    }
+}
